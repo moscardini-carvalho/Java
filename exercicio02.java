dd0507f40e9250ee73c28efc13dbeb402be67243
João Pedro <162808430+moscardini-carvalho@users.noreply.github.com>
2024-08-30 22:22:17 -0300

    Add files via upload

---
+++ exercicio02.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author 1091392413018
+ */
+public class exercicio02 {
+    public static void main(String[] args) {
+        System.out.println("1, 2, 3, 4");
+        
+        System.out.print("1 ");
+        System.out.print("2 ");
+        System.out.print("3 ");
+        System.out.print("4");
+        
+        System.out.printf("%n%d, %d, %d, %d", 1, 2, 3, 4);
+    }
+}
